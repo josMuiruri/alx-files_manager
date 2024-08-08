5e546b44e9e404a2786a036776aa75acb2b01e56
josMuiruri <josemuiruri51@gmail.com>
2024-08-08 04:05:29 +0300

    constructor creates a client to MongoDB: host, port, db

--- utils/db.js
+++ utils/db.js
@@ -1,12 +1,12 @@
 import { MongoClient } from "mongodb";
 
-const host = process.env.DB_HOST || "localhost";
-const port = process.env.DB_PORT || 27017;
-const database = process.env.DB_DATABASE || "files_manager";
-const url = `mongodb://${host}:${port}`;
-
 class DBClient {
   constructor() {
+    const host = process.env.DB_HOST || "localhost";
+    const port = process.env.DB_PORT || 27017;
+    const database = process.env.DB_DATABASE || "files_manager";
+    const url = `mongodb://${host}:${port}`;
+
     this.client = new MongoClient(url, { useUnifiedTopology: true });
     this.client
       .connect()
