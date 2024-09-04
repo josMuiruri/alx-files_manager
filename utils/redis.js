6e4938eebe1a8fb449024449df33cc0505766f4b
josMuiruri <josemuiruri51@gmail.com>
2024-09-04 04:33:46 +0300

    Redis utils

---
+++ utils/redis.js
@@ -0,0 +1,56 @@
+import { createClient } from 'redis';
+
+class RedisClient {
+  // create a new redis instance
+  constructor() {
+    this.client = createClient();
+    this.client.on('error', (err) =>
+      console.error('Redis client not connected to the server:', err)
+    );
+  }
+
+  // checks if the client connection to redis is active
+
+  isAlive() {
+    return this.client.connected;
+  }
+
+  async get(key) {
+    return new Promise((resolve, reject) => {
+      this.client.get(key, (err, value) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(value);
+        }
+      });
+    });
+  }
+
+  async set(key, value, duration) {
+    return new Promise((resolve, reject) => {
+      this.client.set(key, value, 'EX', duration, (err) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve();
+        }
+      });
+    });
+  }
+
+  async del(key) {
+    return new Promise((resolve, reject) => {
+      this.client.del(key, (err) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve();
+        }
+      });
+    });
+  }
+}
+
+const redisClient = new RedisClient();
+export default redisClient;
